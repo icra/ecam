bd1a9af7e51a02e17285b96d37343b24566d2bcc
lbosch <lbosch@icra.cat>
2015-11-03 19:18:26 +0100

    polint comments

--- global.js
+++ global.js
@@ -11,20 +11,20 @@ var Global =
 	comments:"",			//comments
 
 	//GLOBAL
-	gV1:31,				//Assessment period											(days)
-	gV2:108239,			//Energy costs												(Euro)
-	gV3:635242,			//Running costs												(Euro)
-	gV4:1501779,		//Total energy consumed										(kWh)
-	gV5:368475,			//Resident population within the utility area of service	(Inhab)
-	gV6:0.225,			//Energy mix consumed										(CO2/kWh)
+	gV1:31,				//Assessment period (days)
+	gV2:108239,			//Energy costs (Euro)
+	gV3:635242,			//Running costs (Euro)
+	gV4:1501779,		//Total energy consumed (kWh)
+	gV5:368475,			//Resident population within the utility area of service (Inhab)
+	gV6:0.225,			//Energy mix consumed (CO2/kWh)
 
 	gE1:function()		//Energy Cost Ratio
 	{return this.gV2/this.gV3},
 
-	gE2:function()		//Per capita energy consumption 							(kWh/Inhab/year)
+	gE2:function()		//Per capita energy consumption (kWh/Inhab/year)
 	{return this.gV4*365/this.gV1/this.gV5},
 
-	GHG:function()		//Per capita GHG emissions									(CO2e kg/ inInhab/year)	
+	GHG:function()		//Per capita GHG emissions (CO2e kg/ inInhab/year)	
 	{
 		return (this.gV4*this.gV6) //TODO
 	}
