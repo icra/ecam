0ecb424cdda307e5331c775dee9402f1473c3600
lbosch <lbosch@icra.cat>
2015-11-25 19:53:57 +0000

    added logos v2

---
+++ logos.php
@@ -0,0 +1,14 @@
+
+<div>
+	<style>
+		img.logo{
+			width:150px;
+		}
+	</style>
+	<!--iwa-->
+	<img class=logo src=img/logo-iwa.jpg>
+	<!--deutsche gesellschaft fur internationale zusammenarbeit gmbh-->
+	<img class=logo src=img/logo-giz.png>
+	<!--federal ministry for environment of Germany-->
+	<img class=logo src=img/logo-fmg.png>
+</div>
