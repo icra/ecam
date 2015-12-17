56283a989da642fb878ecd5df7926751949cb404
lbosch <lbosch@icra.cat>
2015-12-17 03:20:26 +0100

    updated about.php

--- about.php
+++ about.php
@@ -6,5 +6,11 @@
 <!--NAVBAR--><?php include"navbar.php"?>
 <!--TITLE--><h1>About</h1>
 
-<div> TBD </div>
-<div>Contact Form?</div>
+<div style=max-width:40%;text-align:justify;line-height:2em>
+	The source code is in <a href=https://github.com/holalluis/ecam>GitHub</a>.<br>
+	From there you can download the tool and have it offline. <br>
+	You will need a localhost PHP to make it work, such as <a href=https://www.apachefriends.org/index.html>XAMPP</a>.<br>
+	This software was written entirely using the <a href=http://www.vim.org>Vim</a> editor, inside a <a href=https://www.cygwin.com/>Cygwin</a> terminal. <br>
+	Tool coded in Javascript, PHP &amp; HTML languages. <br>
+	Developed by Lluis Bosch (<a href=http://icra.cat>ICRA</a>) in collaboration with <a href=http://www.iwa-network.org/>IWA</a>.
+</div>
