ab357a2a9bdba814bba270b0f42955bbf59e93c1
lbosch <lbosch@icra.cat>
2016-02-02 01:00:49 +0100

    better visuals for tablets and smartphones

--- imports.php
+++ imports.php
@@ -4,7 +4,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="">
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
 
 <!-- Disable tap highlight on IE -->
 <meta name="msapplication-tap-highlight" content="no">
