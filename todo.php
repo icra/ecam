3779691e27882d050e47f17d3bca9c1be9823975
lbosch <lbosch@icra.cat>
2016-02-20 17:59:59 +0100

    update todo list

--- todo.php
+++ todo.php
@@ -14,13 +14,14 @@
 	<tr><th>Task<th>Status
 	<tr><td colspan=2 class=taskgroup>BACK-END
 		<tr><td>Prepare a list of variables that need to be visible in L2 from L1<td>Wait</td>
-		<tr><td>Solve DQ.hasEstimatedData() for wGHGx_L2 <td>Next task to do</td>
+		<tr><td>Service levels<td>Wait</td>
 	<tr><td colspan=2 class=taskgroup>FRONT-END
 		<tr><td>PI units be changed by user via dropdown menu<td>Difficult</td>
 		<tr><td>Create Warning in new system button<td>Pending</td>
 		<tr><td>Implement question behaviour from the word file Mario sent <td>Pending</td>
 		<tr><td>Next/back buttons at each stage<td>Pending</td>
 		<tr><td>Create a template for popup menus (to be used with descriptions of parameters) <td>Pending</td>
+		<tr><td>Show data quality in detailed info (for inputs) <td>Pending</td>
 </table>
 <!--FOOTER--><?php include'footer.php'?>
 <script>
