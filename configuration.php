ca71d6dfa04926003f51394c81a42273039fe236
lbosch <lbosch@icra.cat>
2016-03-07 14:35:23 +0000

    you cannot go next from configuration if nothing selected

--- configuration.php
+++ configuration.php
@@ -419,11 +419,12 @@
 		function nextPage()
 		{
 			event.stopPropagation();
+			if(Global.Configuration['Active Stages'].water==0 && Global.Configuration['Active Stages'].waste==0)
+			{
+				alert("There are no active stages. Click on the left table to activate them.");
+				return;
+			}
 			window.location="birds.php"; return;
-
-			if(Global.Configuration['Active Stages'].water==1){window.location="edit.php?level=Water";return;}
-			if(Global.Configuration['Active Stages'].waste==1){window.location="edit.php?level=Waste";return;}
-			alert("ERROR! There are no active stages. Click on the left table to activate them.");
 		}
 	</script>
 	<button class="button next" onclick=nextPage()>Next</button>
