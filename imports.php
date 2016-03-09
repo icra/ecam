1770c1efeae1ff08ad002b4106777d40e2ff1d53
lbosch <lbosch@icra.cat>
2016-03-09 21:45:23 +0100

    changed imports

--- imports.php
+++ imports.php
@@ -3,18 +3,9 @@
 <link rel="stylesheet" href="css.css">
 
 <!--things for mobiles-->
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="">
-<!--
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
--->
-<meta name="msapplication-tap-highlight" content="no">
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="application-name" content="Comics">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="Comics">
-<meta name="theme-color" content="#2F3BA2">
+<meta name="theme-color" content="#00aff1">
 
 <!--libraries here-->
 <script src="js/lz-string.js"></script>
