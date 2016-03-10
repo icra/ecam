84c9133c0bb68a708048277f20d40058ba7e2a3c
lbosch <lbosch@icra.cat>
2016-03-10 21:53:00 +0100

    updated problem list

--- problems.php
+++ problems.php
@@ -5,61 +5,61 @@
 		#problems table{display:inline-block;vertical-align:top;margin:0.5em}
 		#problems tr:hover {background:lightcoral}
 	</style>
+	<script>
+		//get unused inputs
+		function getUnused(obj)
+		{
+			var unused=[];
+			for(var field in obj)
+			{
+				var type = typeof(obj[field]);
+				switch(type)
+				{
+					case 'number':
+						var n=Formulas.outputsPerInput(field).length;
+						if(n==0) unused.push(field);
+						break;
+					case 'function':
+						if(field.search(/^c_/)==0)
+						{
+							var n=Formulas.outputsPerInput(field).length;
+							if(n==0) unused.push(field);
+						}
+						break;
+					case 'object':
+						unused=unused.concat(getUnused(obj[field]));
+						break;
+				}
+			}
+			return unused;
+		}
+
+		//count all variables (inputs and outputs)
+		function countVariables(obj)
+		{
+			var n=0;
+			for(var field in obj)
+			{
+				var type=typeof(obj[field]);
+				switch(type)
+				{
+					case 'number':
+					case 'function':
+						n++
+						break;
+					case 'object':
+						n+=countVariables(obj[field])
+						break;
+				}
+			}
+			return n;
+		}
+	</script>
 </head><body><center>
 <!--sidebar--><?php include'sidebar.php'?>
 <!--NAVBAR--><?php include"navbar.php"?>
 <!--linear--><?php include'linear.php'?>
 <!--TITLE--><h1>Automatic finding of potential sources of Problems (debug mode)</h1>
-<script>
-	//get unused inputs
-	function getUnused(obj)
-	{
-		var unused=[];
-		for(var field in obj)
-		{
-			var type = typeof(obj[field]);
-			switch(type)
-			{
-				case 'number':
-					var n=Formulas.outputsPerInput(field).length;
-					if(n==0) unused.push(field);
-					break;
-				case 'function':
-					if(field.search(/^c_/)==0)
-					{
-						var n=Formulas.outputsPerInput(field).length;
-						if(n==0) unused.push(field);
-					}
-					break;
-				case 'object':
-					unused=unused.concat(getUnused(obj[field]));
-					break;
-			}
-		}
-		return unused;
-	}
-
-	//count all variables (inputs and outputs)
-	function countVariables(obj)
-	{
-		var n=0;
-		for(var field in obj)
-		{
-			var type=typeof(obj[field]);
-			switch(type)
-			{
-				case 'number':
-				case 'function':
-					n++
-					break;
-				case 'object':
-					n+=countVariables(obj[field])
-					break;
-			}
-		}
-		return n;
-	}
-</script>
 
 <div id=problems>
 	<table>
@@ -114,57 +114,57 @@
 				})
 		</script>
 	</table>
-</div>
 
-<table>
-	<tr><td style=font-weight:bold>Problem 3: INEXISTING CODES IN QUESTIONS
-	<tr><th>Code
-	<script>
-		(function()
-		{
-			var inex = [];
-			//go over questions
-			for(var question in Questions)
+	<table>
+		<tr><td style=font-weight:bold>Problem 3: INEXISTING CODES IN QUESTIONS
+		<tr><th>Code
+		<script>
+			(function()
 			{
-				for(var i in Questions[question])
+				var inex = [];
+				//go over questions
+				for(var question in Questions)
 				{
-					var field = Questions[question][i]
-					if(locateVariable(field)==false)
-						inex.push(field)
+					for(var i in Questions[question])
+					{
+						var field = Questions[question][i]
+						if(locateVariable(field)==false)
+							inex.push(field)
+					}
 				}
-			}
-			return inex;
-		})().forEach(function(code)
-		{
-			document.write("<tr><td>"+code);
-		})
-	</script>
-</table>
-
-<table>
-	<tr><td style=font-weight:bold>Problem 3: INEXISTING QUESTIONS IN CFG
-	<tr><th>Code
-	<script>
-		(function()
-		{
-			var inex = []
-
-			for(var question in Questions)
+				return inex;
+			})().forEach(function(code)
 			{
-				if(typeof(Questions[question])=='function')
-					continue
-				if(Global.Configuration["Yes/No"].hasOwnProperty(question))
-					continue
-				else
-					inex.push(question)
-			}
-			return inex;
-		})().forEach(function(question)
-		{
-			document.write("<tr><td>"+question)
-		})
-	</script>
-</table>
+				document.write("<tr><td>"+code);
+			})
+		</script>
+	</table>
+
+	<table>
+		<tr><td style=font-weight:bold>Problem 3: INEXISTING QUESTIONS IN CFG
+		<tr><th>Code
+		<script>
+			(function()
+			{
+				var inex = []
+
+				for(var question in Questions)
+				{
+					if(typeof(Questions[question])=='function')
+						continue
+					if(Global.Configuration["Yes/No"].hasOwnProperty(question))
+						continue
+					else
+						inex.push(question)
+				}
+				return inex;
+			})().forEach(function(question)
+			{
+				document.write("<tr><td>"+question)
+			})
+		</script>
+	</table>
+</div>
 
 <!--FOOTER--><?php include'footer.php'?>
 <!--CURRENT JSON--><?php include'currentJSON.php'?>
