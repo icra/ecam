b04135f76eb102a22f9f130b011b8f7ff7c5cc87
lbosch <lbosch@icra.cat>
2016-06-13 17:05:55 +0100

    help fixed

--- help.php
+++ help.php
@@ -5,7 +5,5 @@
 <!--sidebar--><?php include'sidebar.php'?>
 <!--NAVBAR--><?php include"navbar.php"?>
 <!--TITLE--><h1><?php write('#help')?></h1>
-
 <p>This page will have resources (example json files and a user manual)</p>
-
 <!--footer--><?php include'footer.php'?>
