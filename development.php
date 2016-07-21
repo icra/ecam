b180f4b31c2cb71d470876a52c91622047593e52
lbosch <lbosch@icra.cat>
2016-07-21 19:45:15 +0100

    new json validation tool and new questions proposed by andres

--- development.php
+++ development.php
@@ -8,6 +8,7 @@
 <div id=main>
 
 <table>
+	<tr><td><a href=validate.php>Current JSON file</a>
 	<tr><td><a href=graph.php?g=graph1>Graph placeholder</a>
 	<tr><td><a href=problems.php>Problem finder</a>
 	<tr><td><a href=constants.php>All constants</a>
