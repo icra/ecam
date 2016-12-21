7d701569f467930b6d8f6505ef7cf88b261dd68c
lbosch <lbosch@icra.cat>
2016-12-21 15:47:41 +0000

    update development

--- development.php
+++ development.php
@@ -8,12 +8,12 @@
 <div id=main>
 
 <table style=font-size:18px>
-	<tr><td><a href=problems.php>Debugging utility</a>
-	<tr><td><a href=validate.php>Current JSON file</a>
-	<tr><td><a href=viewer.php>Data structures</a>
-	<tr><td><a href=benchmark.php>Benchmarking</a>
-	<tr><td><a href=bugs.txt>Bugs</a>
-	<tr><td><a href=graph.php?g=graph1>Graphs</a>
+	<tr><td><a href="problems.php">Debugging utility</a>
+	<tr><td><a href="validate.php">Current JSON file</a>
+	<tr><td><a href="viewer.php">Data structures</a>
+	<tr><td><a href="benchmark.php">Benchmarking</a>
+	<tr><td><a href="bugs.txt">Bugs</a>
+	<tr><td><a href="graph.php?g=graph1">Graphs</a>
 </table>
 
 </div>
