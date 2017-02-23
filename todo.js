b38aa01ea55dca60c2a8ed175e95b27319bd4a62
lbosch <lbosch@icra.cat>
2017-02-23 01:04:26 +0100

    update todo list

--- todo.js
+++ todo.js
@@ -3,9 +3,8 @@ var TODO = {
 	Front://front-end
 	[
 		"[PENDING]     Create cookies to remember folding of sections",
-		"[PENDING]     Cells in substage outputs smaller (to match inputs above)",
+		"[PENDING]     Join tables inputs+outputs for substages to match inputs above",
 		"[PENDING]     Discuss graphs with IWA",
-		"[WAITING IWA] Country data for configuration (EF electricity)",
 		"[WAIT]        Translation after v2 in english is complete",
 		"[WAIT]        Example json file after I/O structure does not change anymore",
 		"[IDEA]        Water flow diagram (in 'graphs.js', 'sankey' function)",
@@ -13,8 +12,10 @@ var TODO = {
 	],
 	Back://back-end
 	[
+		"[PENDING] continue with jose pdf",
 		"[PENDING] IPCC table 6.4 add list of BOD g/person/day",
 		"[PENDING] DO LAST - wsa,wst,wsd,wwc - pink table for pump efficiency equations in 'mail.txt'",
+		"[WAITING IWA] Country data for configuration (EF electricity)",
 		"[WAITING IWA] Infiltration and inflow is not clear (WW Collection)",
 		"[WAITING IWA] CH4 emission factor for 'Anaerobic Lagoon covered' treatment type. see all <a href=variable.php?id=c_wwt_ch4_efac>here</a>",
 		"[WAITING IWA] Formula for <a href=variable.php?id=wwd_KPI_nrg_sav>wwd_KPI_nrg_sav</a>",
