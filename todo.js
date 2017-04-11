d68dab9e09f31d88d4e0d9db15da278dc2b0cb58
lbosch <lbosch@icra.cat>
2017-04-11 17:31:03 +0100

    updated todo list

--- todo.js
+++ todo.js
@@ -3,14 +3,13 @@ var TODO = {
 	Front://front-end
 	[
 		"[WAIT] IWA did not specify graphs",
-		"[WAIT] Translation after v2 in english is complete",
 		"[WAIT] Add in the help page an example json file, after I/O structure does not change anymore",
 		"[WAIT] Sankey diagram (all water flows) (at 'graphs.js')",
 		"[WAIT] Add references (page,chapter,book) to equations (IPCC, BEAM, ...?)",
 	],
 	Back://back-end
 	[
-		"[WAITING IWA] kWh to CO2 factor for remaining countries",
+		"[WAIT] Translation after v2 in english is complete",
 	],
 };
 
