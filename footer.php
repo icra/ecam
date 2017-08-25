fe54ccd2a4e15ab91da3893f9fbbfa7a13a05e8d
pavlovicm <mpavlovicdesign@gmail.com>
2017-08-25 13:45:24 +0200

    index and about page edit

--- footer.php
+++ footer.php
@@ -2,33 +2,36 @@
 
 <style>
 	#footer {
-		text-align:center;
-		padding:0.5em 2em 0.5em 2em;
+		text-align:left;
+		padding:0.5em 10em 0.5em 10em;
 		background:white;
 		line-height:2em;
 		color:white;
 		color:#333;
-		overflow:hidden;
 		height: 6em;
 		bottom:0;
+		margin-top: 50px;
 	}
 	#footer a, #footer a:visited {
 		color:white;
 		color:#333;
 		color:#00aff1;
 	}
+	.footer_wrap #footer{
+		text-align: center;
+	}
 </style>
 
 <footer id=footer >
-	<img src="img/logo-wacclim.jpg" style=height:25px;vertical-align:middle>
-	Copyright <?php echo date("Y")?> 
-	·
+	<img src="img/logo-wacclim.jpg" style=height:70px;vertical-align:middle>
+	<div>Copyright <?php echo date("Y")?> </div>
+	<!-- ·
 	<a href="http://wacclim.org/" target=_blank>WaCCliM</a>
 	·
 	<a href=about.php><?php write("#about")?></a>
 	·
 	<a href=help.php><?php write("#help")?></a>
 	·
-	<a href=mailto:info@wacclim.org><?php write("#contact")?>: info@wacclim.org</a>
+	<a href=mailto:info@wacclim.org><?php write("#contact")?>: info@wacclim.org</a> -->
 </footer>
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html><html><head>
 	<?php include'imports.php'?>
 	<script>function init(){updateResult()}</script>
-</head><body onload=init() style=background:#bce3f8>
+</head><body onload=init() style=background:#fff>
 <!--sidebar--><?php include'sidebar.php'?>
 <!--navbar--><?php include'navbar.php'?>
 <!--main-->
@@ -10,46 +10,67 @@
 		<style>
 			#content *:not(button){margin:0;padding:0;padding-top:0.7em;padding-bottom:0.7em}
 			#content {
-				padding:0 25em 0 10em;
+			    padding: 0 10em 0 10em;
 				text-align:left;
+			}
+			#content .width_50 .img_resp{
+				width: 100%;
+				padding-top: 9em;
 			}
 		</style>
 
 		<!--new title-->
-		<h1 style=font-size:32px>
-			Assess you utility's carbon footprint,<br>
-			energy consumption and service levels
-		</h1>
+		<div class="width_50">
+			<h1 style=font-size:32px>
+				Assess you utility's carbon footprint,<br>
+				energy consumption and service levels
+			</h1>
 
-		<!--new text-->
-		<h4 style=font-size:18px;color:#58595b>
-			The Energy Performance and Carbon Emissions Assessment and Monitoring Tool is <br>
-			designed for assessing the carbon emissions that utilities can control within the<br>
-			urban water cycle and prepare these utilities for future reporting needs on climate<br>
-			mitigation.
-		</h4>
-		<h4 style=font-size:18px;color:#58595b>
-			ECAM is a free and 
-			<a target=_blank href=//github.com/holalluis/ecam>open source</a>
-			tool developed as part of the
-			<a target=_blank href=http://wacclim.org/>WaCCliM project</a>.
-		</h4>
+			<!--new text-->
+			<h4 style=font-size:18px;color:#58595b>
+				The Energy Performance and Carbon Emissions Assessment
+				and Monitoring Tool is designed for assessing the carbon
+				emissions of the urban water cycle that are within the
+				operational boundaries of water and wastewater utilities and
+				prepare these utilities for future reporting needs on climate
+				mitigation.
+			</h4>
+			<h4 style=font-size:18px;color:#58595b>
+				ECAM is a free and 
+				<a target=_blank href=//github.com/holalluis/ecam>open source</a>
+				tool developed as part of the
+				<a target=_blank href=http://wacclim.org/>WaCCliM project</a>.
+			</h4>
 
-		<h4 style=font-size:18px;color:#58595b>
-			First time using ECAM? 
-			Click on 'New' or learn more in <a href=about.php target=_blank>About</a>.
-		</h4>
+			<h4 style=font-size:18px;color:#58595b>
+				First time using ECAM? 
+				Click on 'New' or learn more <a href=about.php target=_blank>here</a>.
+			</h4>
 
-		<!--MENU--><div style="padding:1em 0"><?php include'menu.php'?></div>
+			<!--MENU--><div style="padding:1em 0"><?php include'menu.php'?></div>
 
-		<!--chrome warning-->
-		<div> 
-			<b><?php write('#index_chrome_warning')?></b>
+			<!--chrome warning-->
+			<div> 
+				<b><?php write('#index_chrome_warning')?></b>
+			</div>
+			<div>
+				<a href="http://wacclim.org/" target=_blank>WaCCliM</a> - <a href=about.php><?php write("#about")?></a> - <a href=help.php><?php write("#help")?></a> - <a href=mailto:info@wacclim.org><?php write("#contact")?>: info@wacclim.org</a>
+			</div>
+			<img class="license_img" src="img/cc_icon_white_x2.png" alt="">
+			<img class="license_img" src="img/attribution_icon_white_x2.png" alt="">
+			<img class="license_img" src="img/sa_white_x2.png" alt="">
+			<br>
+			ECAM by IWA and GIZ, implemented by ICRA for WaCCliM Project* is licensed under a <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+			<div>
+				* WaCCliM project is part of the International Climate Initiative (IKI). The German Federal Ministry for the Environment, Nature
+				Conservation, Building and Nuclear Safety (BMUB) supports this initiative on the basis of a decision adopted by the German
+				Bundestag.
+			</div>
 		</div>
 
 		<!--picture-->
-		<div style="padding:0;text-align:center">
-			<img src=svg/diagram.png style=width:70%>
+		<div class="width_50" style="padding:0;text-align:center">
+			<img class="img_resp" src="svg/diagram.png">
 		</div>
 	</div>
 </div>
@@ -57,8 +78,8 @@
 <?php include'footer.php'?>
 
 <!--logos--> 
-<div style=background:white;text-align:center> 
-	<img src=img/logos.png width=40%> 
+<div style="background:white;text-align:left;padding:0em 10em;margin-top:50px;"> 
+	<img src=img/logos.png width=60%> 
 </div>
 
 <!--CURRENT JSON--><?php include'currentJSON.php'?>
