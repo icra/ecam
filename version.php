98a7b422ba78490316ac759c351a2e9bd6af4332
Lluís Bosch <holalluis@gmail.com>
2018-05-17 18:56:53 +0200

    link to commits from version number changed

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <div
-onclick="window.open('https://github.com/icra/ecam');"
+onclick="window.open('https://github.com/icra/ecam/commits/master');"
 style="font-size:smaller;font-family:monospace;"
 >
 version 2.1 alpha
