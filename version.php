e901f86bb341b1810ba0b54d1c662750e016d52a
holalluis <holalluis@gmail.com>
2018-05-23 00:00:50 +0200

    update version comment

--- version.php
+++ version.php
@@ -1,6 +1,7 @@
 <div
 onclick="window.open('https://github.com/icra/ecam/commits/master');"
 style="font-size:smaller;font-family:monospace;"
+title="'Alpha' means that the testing is done by the developers. 'Beta' means that the testing is done by people other than the developers."
 >
 version 2.1 alpha
 </div>
