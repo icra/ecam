68d7d67e6301cdec3467dfe4a3f6475b39100330
holalluis <holalluis@gmail.com>
2018-09-28 23:30:09 +0200

    solved issue #186

--- utils.js
+++ utils.js
@@ -133,11 +133,13 @@ function getVariable(code){
 /*convert number to formated string: i.e. "3.999,4" instead of 3999.4*/
 function format(number,digits){
   //default digits if not specified
-  digits=digits||2;
+  digits=digits||0;
 
   //default digits for values
-  if     (number>1e4)  digits=0;
-  else if(number<0.01) digits=4;
+  if(number<1000) digits=1;
+  if(number< 100) digits=2;
+  if(number<0.10) digits=3;
+  if(number<0.01) digits=4;
 
   //for non applicable
   if(number=="NA"){ return "<span style=color:#ccc>NA</span>"; }
