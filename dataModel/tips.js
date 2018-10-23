7dbd35f82e28578743617b29b314faf9bd5c8f52
holalluis <holalluis@gmail.com>
2018-10-23 21:31:06 +0200

    tips translated issue #21"

--- dataModel/tips.js
+++ dataModel/tips.js
@@ -1,6 +1,6 @@
 /* Random tips */
 var Tips=[
-	"Use the TAB key (&rarrb;) to input values faster",
+	"Use the TAB key to input values faster",
 	"Double click a graph to download it",
 	"Export results to Excel at Main Menu/Other/Export",
 	"Press &uarr; and &darr; when editing inputs to quickly increase/decrease the number",
@@ -11,10 +11,14 @@ var Tips=[
 	"Move the mouse over an output to see its formula",
 ];
 
-Tips.random=function() {
-	var leng=Tips.length;
-	var rand=Math.floor(Math.random()*leng);
-	var tip=Tips[rand];
+//translate tips
+Tips.forEach((tip,i)=>{Tips[i]=translate(tip);});
+
+//get a random tip
+Tips.random=function(){
+	let leng=Tips.length;
+	let rand=Math.floor(Math.random()*leng);
+	let tip=Tips[rand];
 	if(leng>1){Tips.splice(rand,1);}
 	return tip;
 }
