cac03e6c80b3e59bc3848c38d26a961498f1cae3
Lluís Bosch <holalluis@gmail.com>
2021-01-11 12:13:15 +0100

    update

--- v3/dev/deploy_to_dev_server.sh
+++ v3/dev/deploy_to_dev_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#git
+git add *
+git commit -m 'update'
+git push
+
 # deploy github repo to dev server
 command='cd /var/www/vhosts/h2793818.stratoserver.net/ecam-dev.h2793818.stratoserver.net; git pull'
 ssh root@h2793818.stratoserver.net "$command"
