ee3a0d4cf194015db06386542c9d9a128853bae4
Lluís Bosch <holalluis@gmail.com>
2021-06-15 22:21:03 +0200

    border iframe 0

--- v3/frontend/components/docs.js
+++ v3/frontend/components/docs.js
@@ -1,18 +1,19 @@
-let docs = new Vue({
+let docs=new Vue({
   el:"#docs",
-
   data:{
     visible:false,
     Languages,
   },
 
-  methods:{},
-
   template:`
     <div id=docs v-if="visible && Languages.ready">
       <iframe
         src="frontend/docs/"
-        style="width:100%;height:1000px"
+        style="
+          width:100%;
+          height:1000px;
+          border:none;
+        "
       ></iframe>
     </div>
   `,
